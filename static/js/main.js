94ec668ef62b422814f7b3f96f6a6be475ea7a82
Shehanford <shehanrford@gmail.com>
2025-11-15 11:11:17 +0530

    Initial portfolio site

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+// Smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+    a.addEventListener('click', e=>{
+      e.preventDefault();
+      const target = document.querySelector(a.getAttribute('href'));
+      if(target) target.scrollIntoView({behavior:'smooth', block:'start'});
+    });
+  });
+  
